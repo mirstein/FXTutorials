eac6e41ecc24ec8038eab1d3fc311de4d39f9d69
msteinle <mirko.steinle@edu.ge.ch>
2019-11-20 12:09:10 +0100

    A working config

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,10 @@
 module fxtutorials {
 
     requires javafx.controls;
-
     requires javafx.fxml;
+    requires javafx.swing;
+    requires java.desktop;
 
+    opens com.almasb.calc to javafx.fxml;
+    exports com.almasb.calc;
 }
